7ca72386d84581e27420c6b6e4509aff8b02cf8e
FandyyyDev <nazril.afandi1603@gmail.com>
2022-04-08 21:40:00 +0700

    t

---
+++ command/config.js
@@ -0,0 +1,41 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+//━━━━━━━━━━━━━━━[ WEBSITE API ]━━━━━━━━━━━━━━━━━//
+
+global.APIs = {
+zeroyt7: 'https://zeroyt7-api.xyz',
+}
+
+//━━━━━━━━━━━━━━━[ APIKEY WEBSITE API ]━━━━━━━━━━━━━━━━━//
+
+global.APIKeys = {
+'https://zeroyt7-api.xyz': 'yourkey',
+}
+
+//━━━━━━━━━━━━━━━[ OTHER ]━━━━━━━━━━━━━━━━━//
+
+global.ownername = 'Juvian'
+global.owner = ['6282362285077']
+global.packname = 'Juv Bot'
+global.author = '© Juvian'
+global.prefa = ['','!','.']
+global.sp = '⭔'
+global.mess = {
+admin: 'Fitur Khusus Admin Group!',
+botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+owner: 'Fitur Khusus Owner Bot',
+eror: 'Yah eror',
+group: 'Fitur Digunakan Hanya Untuk Group!',
+private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+bot: 'Fitur Khusus Pengguna Nomor Bot',
+wait: 'Loading...',
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update'${__filename}'`))
+delete require.cache[file]
+require(file)
+})
